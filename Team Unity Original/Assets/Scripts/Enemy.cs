19cc20941367377e4bbc48cbd22e2edb897623bb
Mishy34 <63585101+Mishy34@users.noreply.github.com>
2021-06-14 22:17:37 +0530

    Completed game with build files
    Please find the build folder for the runnable files for windows and mac (Just double click to enjoy the game)

---
+++ Team Unity Original/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected Animator anim;
+    protected Rigidbody2D rb;
+    private AudioSource death;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        death = GetComponent<AudioSource>();
+    }
+
+    public void JumpedOn()
+    {
+        anim.SetTrigger("Death");
+        death.Play();
+        GetComponent<Collider2D>().enabled = false;
+    }
+
+    public void Death()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Team Unity Original/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    //Start() variables
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+
+    //FSM
+    private enum State {idle, running, jumping, falling, hurt}
+    private State state = State.idle;
+
+    //Variable to pass final score
+    private LevelWin levelWin;
+
+    //Inspector variables
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private float hurtForce = 3f;
+    [SerializeField] private AudioSource footstep;
+    [SerializeField] private AudioSource collect;
+    [SerializeField] private int collectable = 0;
+    [SerializeField] private TextMeshProUGUI collectableCount;
+    [SerializeField] private int health = 3;
+    [SerializeField] private TextMeshProUGUI healthText;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(state != State.hurt)
+        {
+            Movement();
+        }
+        AnimationState();
+        anim.SetInteger("state", (int)state);   //set animation based on enum state
+    }
+
+    void Awake(){
+
+        levelWin =  GameObject.FindObjectOfType<LevelWin>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Collectable")
+        {
+            collect.Play();
+            Destroy(collision.gameObject);
+            collectable += 1;
+            collectableCount.text = collectable.ToString();
+            levelWin.updateScore(collectable);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
+
+            if(state == State.falling)
+            {
+                enemy.JumpedOn();
+                Jump();
+            }
+            else
+            {
+                state = State.hurt;
+                HandleHealth();
+
+                if(other.gameObject.transform.position.x > transform.position.x)
+                {
+                    //enemy is to my right, therefore I should be damaged and move left
+                    rb.velocity = new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else
+                {
+                    //enemy is to my left, therefore I should be damaged and move right
+                    rb.velocity = new Vector2(hurtForce, rb.velocity.y);
+                }
+            }
+        }
+    }
+
+    private void HandleHealth()
+    {
+        health -= 1;
+        healthText.text = health.ToString();
+
+        if(health <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void Movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        //moving left
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        //moving right
+        else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        // New added start
+        else if(hDirection == 0 && coll.IsTouchingLayers(ground))
+        {
+            if (state == State.running || state == State.falling)
+            {
+                rb.velocity = Vector3.zero;                
+            }
+        }
+        // New added end
+                
+        //jumping
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        state = State.jumping;
+    }
+
+    private void AnimationState()
+    {
+        if(state == State.jumping)
+        {
+            if (rb.velocity.y < 0.1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (state == State.hurt)
+        {
+            if(Mathf.Abs(rb.velocity.x) < 0.1f)
+            {
+                state = State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //moving
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+
+    private void Footstep()
+    {
+        footstep.Play();
+    }
+}
