d4de7e3a9794094179d5364f36da09f995575f61
Mishy34 <63585101+Mishy34@users.noreply.github.com>
2021-06-14 23:37:53 +0530

    Completed game with build files for mac and windows

---
+++ Team Unity Original/Assets/Scripts/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dog : MonoBehaviour
+{
+    public GameObject winScrn ;
+    private void OnCollisionEnter2D(Collision2D other){
+
+        if(other.gameObject.tag == "Player"){
+
+            winScrn.gameObject.SetActive(true);
+        }
+    }
+}
